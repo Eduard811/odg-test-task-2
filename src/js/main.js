07d2e423323dabbf3916a9c845b7d571507789de
Eduard811 <eduard.albertyan1@mail.ru>
2021-05-17 21:31:43 +0300

    fixed modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
 const popup = document.querySelector('.b-popup')
 const button = document.querySelector('[data-popup-button]')
 
-button.addEventListener('click', () => popup.classList.toggle('b-popup_active'))
+button.addEventListener('click', () => {
+  popup.classList.toggle('b-popup_active')
+  document.body.style.overflow = 'hidden'
+  document.body.style.position = 'fixed'
+  document.body.style.width = '100%'
+  document.body.style.top = '0px'
+})
