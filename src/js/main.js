f362cb82d2d9d4caa94e7fc444c564f3ddacbec0
Eduard811 <eduard.albertyan1@mail.ru>
2021-05-18 13:11:47 +0300

    open or close popup

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,46 @@
-const popup = document.querySelector('.b-popup')
-const button = document.querySelector('[data-popup-button]')
+'use strict'
 
-button.addEventListener('click', () => {
-  popup.classList.toggle('b-popup_active')
-  document.body.style.overflow = 'hidden'
-  document.body.style.position = 'fixed'
-  document.body.style.width = '100%'
-  document.body.style.top = '0px'
+function Popup() {
+  const popup = document.querySelector('.b-popup')
+  const openButton = document.querySelector('[data-popup-button]')
+  const closeButton = document.querySelector('.b-popup__close')
+  const inside = document.querySelector('.b-popup__wrapper')
+  let isOpen = false
+
+  const openOrClose = () => {
+    document.body.style.overflow = isOpen ? 'hidden' : ''
+    document.body.style.position = isOpen ? 'fixed' : ''
+    document.body.style.width = isOpen ? '100%' : ''
+    document.body.style.top = isOpen ? '0px' : ''
+
+    setTimeout(() => {
+      isOpen
+        ? document.addEventListener('click', handleOutsideClick)
+        : document.removeEventListener('click', handleOutsideClick)
+    })
+  }
+
+  const open = () => {
+    popup.classList.add('b-popup_active')
+    isOpen = true
+    openOrClose()
+  }
+
+  const close = () => {
+    popup.classList.remove('b-popup_active')
+    isOpen = false
+    openOrClose()
+  }
+
+  const handleOutsideClick = (event) => {
+    const path = event.path || (event.composedPath && event.composedPath())
+    !path.includes(inside) && close()
+  }
+
+  openButton.addEventListener('click', open)
+  closeButton.addEventListener('click', close)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  Popup()
 })
