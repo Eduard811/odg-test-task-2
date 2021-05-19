e4c717ce520269c26f453daae2b721cc288b255f
Eduard811 <eduard.albertyan1@mail.ru>
2021-05-19 15:26:04 +0300

    calculate

--- src/js/main.js
+++ src/js/main.js
@@ -41,6 +41,82 @@ function Popup() {
   closeButton.addEventListener('click', close)
 }
 
+function inputsGenerator() {
+  const calculateButton = document.querySelector('.b-field__button')
+  const block = document.querySelector('.b-field__block-two')
+  const input = document.querySelector('[data-field-input]')
+  const state = {
+    apartmentСost: 10000000, //стоимость квартиры
+    salary: 0, //зарплата в месяц
+    taxDeduction: 0, // налоговый вычет за год
+    maxTaxDeduction: 0, // максимальный налоговый вычет
+    checkBoxs: [], // суммы для чекбоксов после подсчета
+  }
+
+  const createInputs = () => {
+    if (state.checkBoxs.length > 0) {
+      block.classList.toggle('b-field__block-two_active')
+      block.innerHTML = state.checkBoxs
+        .map((sum, i) => {
+          return `<div class="b-field__checkbox-block">
+            <input
+            type="checkbox"
+            name="total"
+            id="total-checkbox-${i + 1}"
+            class="b-field__checkbox"
+            >
+            <label for="total-checkbox-${i + 1}" class="b-field__checkbox-label">${sum} рублей <span>в ${
+            i + 1
+          }-ый год</span></label>
+            </div>`
+        })
+        .join('')
+    } else {
+      block.innerHTML = ''
+      block.classList.remove('b-field__block-two_active')
+    }
+  }
+
+  const onChangeInput = (event) => {
+    state.salary = event.target.value
+    let result = calculateRecoupment()
+    if (result) {
+      state.checkBoxs = result
+    }
+  }
+
+  const calculateRecoupment = () => {
+    if (state.salary >= 10000) {
+      state.apartmentСost > 2000000
+        ? (state.maxTaxDeduction = 260000)
+        : (state.maxTaxDeduction = Math.round((state.apartmentСost / 100) * 13))
+
+      state.taxDeduction = Math.round(((state.salary * 12) / 100) * 13)
+      const newArray = []
+      let acc
+      for (let i = 1; state.taxDeduction * i + 1 <= state.maxTaxDeduction; i++) {
+        newArray.push(state.taxDeduction)
+        acc = i
+      }
+      let max = acc + 2
+      if (state.taxDeduction * max > state.maxTaxDeduction) {
+        let difference = state.taxDeduction * acc - state.maxTaxDeduction
+
+        newArray.push(Math.abs(difference))
+      }
+
+      return newArray
+    } else {
+      state.salary = 0
+      state.checkBoxs.length = 0
+    }
+  }
+
+  input.addEventListener('input', onChangeInput)
+  calculateButton.addEventListener('click', createInputs)
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   Popup()
+  inputsGenerator()
 })
